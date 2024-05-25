1b1aa584cd6fafe1645c95d60dbdea3b2fcc537a
Zhemral <163653846+Zhemral@users.noreply.github.com>
2024-05-25 16:41:03 +0300

    Update task-4.js

--- task-4.js
+++ task-4.js
@@ -1,1 +1,12 @@
-
+javascriptlet student = {
+    name: "John Doe",     // String
+    age: 21,              // Number
+    isGraduated: false,   // Boolean
+    futureGoals: null,    // Null
+    college: undefined,   // Undefined
+    secretKey: Symbol(),   // Symbol
+    courseSubjects: ["Math", "Science", "History"], // Array
+    greet: function() {  // Function (Method)
+        console.log(Hello, my name is ${this.name} and I am ${this.age} years old.);
+    }
+};
