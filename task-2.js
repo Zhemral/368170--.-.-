b4909471c9932d5f029f5253e9f91db98a7e22c9
Zhemral <163653846+Zhemral@users.noreply.github.com>
2024-05-25 16:32:23 +0300

    Update task-2.js

--- task-2.js
+++ task-2.js
@@ -1,1 +1,12 @@
-
+function invertCase(string) {
+    let invertedString = '';
+    for (let i = 0; i < string.length; i++) {
+        let char = string[i];
+        if (char === char.toUpperCase()) {
+            invertedString += char.toLowerCase();
+        } else {
+            invertedString += char.toUpperCase();
+        }
+    }
+    return invertedString;
+}
