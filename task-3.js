74b37d7312398b2dbc3167fe8735f877151ff0e9
Zhemral <163653846+Zhemral@users.noreply.github.com>
2024-05-25 16:33:31 +0300

    Update task-3.js

--- task-3.js
+++ task-3.js
@@ -1,1 +1,13 @@
+function isArmstrongNumber(number) {
+    let sum = 0;
+    let temp = number;
+    let digits = number.toString().length;
 
+    while (temp > 0) {
+        let digit = temp % 10;
+        sum += Math.pow(digit, digits);
+        temp = Math.floor(temp / 10);
+    }
+    
+    return sum === number;
+}
